81b29015c056ec1055eb7beef40b7b6c850aa702
Dmytro Kaplin <dmitry.k@overdose.digital>
2022-07-10 12:58:11 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Admin UI for Customer Prices module
+
+# License
+
+Copyright (c) 2022 ePuzzle contributors.
+The customer price module is [MIT licensed](./LICENSE).
+Project coordinator: &lt;dkaplin1994@gmail.com&gt;
---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'ePuzzle_CustomerPriceAdminUi',
+    __DIR__
+);
